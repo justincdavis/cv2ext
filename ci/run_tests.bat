b83b9494c6474b6aeac1e6e3b52251bc0e78f6f3
justincdavis <davis.justin@mssm.org>
2024-02-21 15:01:04 -0700

    Fix windows test paths

--- ci/run_tests.bat
+++ ci/run_tests.bat
@@ -1,6 +1,8 @@
 @echo off
 setlocal enabledelayedexpansion
 
-python3 -m pytest --log-cli-level=WARNING -rP .\tests\test_same.py || exit /b 1
+python3 -m pytest --log-cli-level=WARNING -rP .\tests\iterable_video\test_read.py || exit /b 1
+python3 -m pytest --log-cli-level=WARNING -rP .\tests\iterable_video\test_same.py || exit /b 1
+python3 -m pytest --log-cli-level=WARNING -rP .\tests\iterable_video\test_sequential.py || exit /b 1
 
 endlocal
