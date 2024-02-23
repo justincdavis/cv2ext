9291b825f1a967320a6e055b84c9d3dcaa289c2f
justincdavis <davis.justin@mssm.org>
2024-02-23 11:23:11 -0700

    Bump version 0.0.3 -> 0.0.4

--- docs/conf.py
+++ docs/conf.py
@@ -14,7 +14,7 @@ import cv2ext
 project = 'cv2ext'
 copyright = '2024, Justin Davis'
 author = 'Justin Davis'
-version = '0.0.3'
+version = '0.0.4'
 
 assert version == cv2ext.__version__
 
--- src/cv2ext/__init__.py
+++ src/cv2ext/__init__.py
@@ -62,6 +62,6 @@ _log = logging.getLogger(__name__)
 from ._iterablevideo import IterableVideo
 
 __all__ = ["IterableVideo"]
-__version__ = "0.0.3"
+__version__ = "0.0.4"
 
 _log.info(f"Initialized cv2ext with version {__version__}")
