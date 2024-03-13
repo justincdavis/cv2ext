7c79f8cc45abc952a1342be085b90314b317fe3a
justincdavis <davis.justin@mssm.org>
2024-03-13 13:36:43 -0600

    update windows test file so windows runs all unit tests on cloud

--- ci/run_tests.bat
+++ ci/run_tests.bat
@@ -1,8 +1,10 @@
 @echo off
 setlocal enabledelayedexpansion
 
-python3 -m pytest --log-cli-level=WARNING -rP .\tests\iterable_video\test_read.py || exit /b 1
-python3 -m pytest --log-cli-level=WARNING -rP .\tests\iterable_video\test_same.py || exit /b 1
-python3 -m pytest --log-cli-level=WARNING -rP .\tests\iterable_video\test_sequential.py || exit /b 1
+@REM python3 -m pytest --log-cli-level=WARNING -rP .\tests\iterable_video\test_read.py || exit /b 1
+@REM python3 -m pytest --log-cli-level=WARNING -rP .\tests\iterable_video\test_same.py || exit /b 1
+@REM python3 -m pytest --log-cli-level=WARNING -rP .\tests\iterable_video\test_sequential.py || exit /b 1
+
+python3 -m pytest -x --log-cli-level=WARNING -rP tests/ || exit /b 1
 
 endlocal
