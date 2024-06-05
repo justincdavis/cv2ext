4a42b89586f5ee48e2a29d0d4a39f758e1e7ec2e
justincdavis <davis.justin@mssm.org>
2024-06-05 15:25:44 -0600

    Allow pytest to continue on failure

--- ci/run_tests.bat
+++ ci/run_tests.bat
@@ -5,6 +5,6 @@ setlocal enabledelayedexpansion
 @REM python3 -m pytest --log-cli-level=WARNING -rP .\tests\iterable_video\test_same.py || exit /b 1
 @REM python3 -m pytest --log-cli-level=WARNING -rP .\tests\iterable_video\test_sequential.py || exit /b 1
 
-python3 -m pytest -x --log-cli-level=WARNING -rP tests/ || exit /b 1
+python3 -m pytest --log-cli-level=WARNING -rP tests/ || exit /b 1
 
 endlocal
