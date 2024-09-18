1852cd96a1a9f8685a3c42d8a35d6100e177edcc
justincdavis <davis.justin@mssm.org>
2024-09-18 12:29:38 -0500

    Bump version 0.0.17 -> 0.0.18

--- docs/conf.py
+++ docs/conf.py
@@ -14,7 +14,7 @@ import cv2ext
 project = 'cv2ext'
 copyright = '2024, Justin Davis'
 author = 'Justin Davis'
-version = '0.0.17'
+version = '0.0.18'
 
 assert version == cv2ext.__version__
 
--- src/cv2ext/__init__.py
+++ src/cv2ext/__init__.py
@@ -235,7 +235,7 @@ __all__ = [
     "tracking",
     "video",
 ]
-__version__ = "0.0.17"
+__version__ = "0.0.18"
 
 _log.info(f"Initialized cv2ext with version {__version__}")
 
