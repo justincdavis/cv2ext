a52f104527b38e86716a1b1cf53dfd8c3fcca2f9
justincdavis <davis.justin@mssm.org>
2024-12-20 10:30:24 -0600

    add mypy stub generate to make_stubs in ci

--- ci/make_stubs.py
+++ ci/make_stubs.py
@@ -11,8 +11,17 @@ def pyright_stubs():
         print(f"Making stubs for {module}")
         subprocess.run(["pyright", "--createstub", module])
 
+
+def mypy_stubs():
+    for module in ["numba"]:
+        print(f"Making stubs for {module}")
+        subprocess.run(["stubgen", "-o", "typings", "-p", module])
+
+
 def main():
-    pyright_stubs()
+    # pyright_stubs()
+    mypy_stubs()
+
 
 if __name__ == "__main__":
     main()
