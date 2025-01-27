ebe9a3e1db30e6cb7a7e1d9ac74d27234eb54463
justincdavis <davis.justin@mssm.org>
2025-01-27 16:04:42 -0600

    Add research submodule and BigLittle impl

---
+++ src/cv2ext/research/__init__.py
@@ -0,0 +1,20 @@
+# Copyright (c) 2024 Justin Davis (davisjustin302@gmail.com)
+#
+# MIT License
+"""
+Research implementations for various papers/methods.
+
+Submodules
+----------
+:module:`biglittle`
+    An implementation of the BigLittle paper.
+
+"""
+
+from __future__ import annotations
+
+from . import biglittle
+
+__all__ = [
+    "biglittle",
+]
